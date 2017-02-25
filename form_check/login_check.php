33f28de8862b5fe006d7599f7849c0f06645be45
qq547276542 <547276542@qq.com>
2017-02-25 13:37:13 +0800

    添加项目配置文件

--- form_check/login_check.php
+++ form_check/login_check.php
@@ -1,24 +1,17 @@
 <html>
 <body>
+  <?php include '../config/database.php';
+        include '../tool/tool.php'
+  ?>
   <?php 
       header('content-type:text/html;charset=utf-8;');//统一字符集，防止乱码
       error_reporting(0);//屏蔽错误信息
-      function test_input($data) {  
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-      }
       $username=$password="";
       if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $username = test_input($_POST["username"]);
         $password = test_input($_POST["password"]);
       }
-      $con = mysql_connect("localhost","upcacm","upcacm");
-      if (!$con)
-      {
-         die('Could not connect: ' . mysql_error());
-      }
+     
        
   ?>
 </body>
--- form_check/sign_up_check.php
+++ form_check/sign_up_check.php
@@ -1,45 +1,38 @@
 <html>
 <body>
+  <?php include '../config/database.php';
+        include '../tool/tool.php'
+  ?>
 	<?php	
       header('content-type:text/html;charset=utf-8;');//统一字符集，防止乱码
       error_reporting(0);//屏蔽错误信息
-      function test_input($data) {  
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-      }
 	    $username=$password=$email="";
 	    if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	      $username = test_input($_POST["username"]);
 	      $email = test_input($_POST["email"]);
 	      $password = test_input($_POST["password"]);
       }
-      $con = mysql_connect("localhost","upcacm","upcacm");
-      if (!$con)
+      $conn = get_connect();
+      if ($conn->connect_error)
       {
-         die('Could not connect: ' . mysql_error());
+         die("connect failed: " . $conn->connect_error);
       }
-      
-      mysql_select_db("acmdb", $con);  
       $sql="INSERT INTO person (username, password, email,usertype)
       VALUES
       ('$username','$password','$email','student')";
       
-      if (!mysql_query($sql,$con))
+      if ($conn->query($sql) === TRUE)
         {
-       // echo "<script type='text/javascript'> alert('该用户名已经存在！'); </script>";
-        echo "<script>history.back();alert('该用户名已经存在！');</script>";
-       // header("Location:../sign_up.php"); 
-       // die('Error: ' . mysql_error());
-        mysql_close($con);
+           echo "<script>alert('success!');</script>";
+           header("Location:../index.php"); 
         }
         else{
-        header("Location:../index.php"); 
-         mysql_close($con);
+        // echo "<script type='text/javascript'> alert('该用户名已经存在！'); </script>";
+           echo "<script>history.back();alert('该用户名已经存在！');</script>";
+        // header("Location:../sign_up.php"); 
+        // die('Error: ' . mysql_error());
       }
-     
-      //注册成功，跳转
+     $conn->close();
        
   ?>
 </body>
