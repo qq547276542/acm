85626c7e15f88698d04a422f4fd28de03103da47
qq547276542 <547276542@qq.com>
2017-03-25 15:10:30 +0800

    添加了oj信息

--- user_info.php
+++ user_info.php
@@ -16,13 +16,13 @@
             </div>
 
             <!-- 做题积分 -->
-            <p style="font-size: 18px;"><span class="glyphicon glyphicon-stats"></span>&nbsp<nobr>做题积分: </nobr> <nobr  class="text-info" > 2333 </nobr> &nbsp (历史最高： <nobr  class="text-info">6666</nobr>)</p>
+            <p style="font-size: 18px;"><span class="glyphicon glyphicon-stats"></span>&nbsp<nobr>做题积分: </nobr> <nobr  class="text-info" > 2333 </nobr> </p>
             <!-- 队伍 --> 
             <p style="font-size: 18px;"><span class="glyphicon glyphicon-tasks"></span>&nbsp<nobr>队伍: </nobr> <nobr  class="text-info" > 世界无dp </nobr></p>
             <!-- 真实姓名 -->
             <p style="font-size: 18px;"><span class="glyphicon glyphicon-user"></span>&nbsp<nobr>真实姓名: </nobr> <nobr  class="text-info" > 刘磊 </nobr></p>
             <!-- 电子邮箱 -->
-            <p style="font-size: 18px;"><span class="glyphicon glyphicon-user"></span>&nbsp<nobr>email: </nobr> <nobr  class="text-info" > 547276542@qq.com </nobr></p>
+            <p style="font-size: 18px;"><span class="glyphicon glyphicon-envelope"></span>&nbsp<nobr>email: </nobr> <nobr  class="text-info" > 547276542@qq.com </nobr></p>
             <!-- 性别 -->
             <p style="font-size: 18px;"><span class="glyphicon glyphicon-record"></span>&nbsp<nobr>性别: </nobr> <nobr  class="text-info" > 男 </nobr></p>
             <!-- T恤尺码 -->
@@ -33,6 +33,7 @@
             <p style="font-size: 18px;"><span class="glyphicon glyphicon-book"></span>&nbsp<nobr>专业: </nobr> <nobr  class="text-info" > 软件工程 </nobr></p>
             <!-- 手机号码 -->
             <p style="font-size: 18px;"><span class="glyphicon glyphicon-earphone"></span>&nbsp<nobr>联系电话: </nobr> <nobr  class="text-info" > 13939212325 </nobr></p>
+            <p style="font-size: 18px;"><span class="glyphicon glyphicon-pencil"></span>&nbsp<nobr>blog: </nobr> <nobr  class="text-info" > <a href="http://blog.csdn.net/qq547276542"> http://blog.csdn.net/qq547276542 </a></nobr></p>
           </div>
           <div class="col-md-4">
            <img src="picture/photo.jpg" alt="您的照片" class="img-thumbnail"/>
@@ -43,8 +44,33 @@
    </div>
 
 
-
  </div>
+</div>
+<div class="row">
+  <div class="col-md-8 col-md-offset-2">
+     <div class="panel panel-default">
+      <div class="panel-heading"><div class="text-info"> 做题情况 </div></div>
+      <div class="panel-body">
+         <table class="table table-bordered table-striped">
+            <tr>
+              <td width="10%"><h4>OJ</h4></td>
+              <td width="10%"><h4>做题数</h4></td>
+              <td width="15%"><h4>用户名</h4></td>
+              <td width="60%"><h4>最近通过</h4></td>
+              <td width="5%"><h4>#</h4></td>
+            </tr>
+            <tr>
+              <td >poj</td>
+              <td >123</td>
+              <td >eason</td>
+              <td ><a href="#">1011</a> <a href="#">1021</a> <a href="#">2021</a></td>
+              <td ><button type="button" class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button></td>
+            </tr>
+         </table>
+         <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span> 添加oj</button>
+      </div>
+     </div>
+  </div>
 </div>
 </body>
 
