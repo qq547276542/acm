74ef95350cfc882d9fce5ef85a58fc62b9c88c96
qq547276542 <547276542@qq.com>
2017-04-10 14:27:39 +0800

    移动端优化

--- nav.php
+++ nav.php
@@ -4,7 +4,10 @@
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-       
+       <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
       </button>
       <a class="navbar-brand" href="#">中国石油大学ACM集训管理平台</a>
     </div>
